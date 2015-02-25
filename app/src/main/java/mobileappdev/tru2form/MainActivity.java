e1660bd24d832e772db9d9d93c293392fe167ace
mitwong <mitwong@umich.edu>
2015-02-25 15:14:46 -0500

    Made a toast to test sendButton

--- app/src/main/java/mobileappdev/tru2form/MainActivity.java
+++ app/src/main/java/mobileappdev/tru2form/MainActivity.java
@@ -1,10 +1,13 @@
 package mobileappdev.tru2form;
 
+import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
@@ -37,5 +40,14 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendText(View view) {
+        Context context = getApplicationContext();
+        CharSequence text = "Text Sent!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
