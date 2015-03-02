f5956968cf6ab62d717ae0117e9010834cb98b02
dancm1 <dancm@umich.edu>
2015-03-02 14:58:06 -0500

    SendSMS v1.0
    Got texting functionality to work, but... not in the most intuitive way
    unfortunately.
    
    Next step is to get the phone numbers from the contacts the user
    defines.
    
    Then, add the tokens that denote the replacement form
    
    Then, parse the strings and give it the ability to loop through and send
    multiple texts

--- app/src/main/java/mobileappdev/tru2form/MainActivity.java
+++ app/src/main/java/mobileappdev/tru2form/MainActivity.java
@@ -7,14 +7,21 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.EditText;
 import android.widget.MultiAutoCompleteTextView;
 import android.widget.Toast;
+import android.view.View.OnClickListener;
+import android.telephony.SmsManager;
 
 import com.android.ex.chips.BaseRecipientAdapter;
 import com.android.ex.chips.RecipientEditTextView;
 
 public class MainActivity extends ActionBarActivity {
 
+    ImageButton button;
+    EditText editPhoneNum;
+    EditText editSMS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +32,37 @@ public class MainActivity extends ActionBarActivity {
                 (RecipientEditTextView) findViewById(R.id.contactsField);
         contactsField.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
         contactsField.setAdapter(new BaseRecipientAdapter(BaseRecipientAdapter.QUERY_TYPE_PHONE, this));
+
+        setContentView(R.layout.activity_main);
+
+        button = (ImageButton) findViewById(R.id.sendTextButton);
+        //editPhoneNum = (EditText) findViewById(R.id.);
+        editSMS = (EditText) findViewById(R.id.editText);
+
+        button.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // String phoneNo = editPhoneNum.getText().toString();
+                String phoneNo = "5179158314";
+                String sms = editSMS.getText().toString();
+
+                try {
+                    SmsManager smsManager = SmsManager.getDefault();
+                    smsManager.sendTextMessage(phoneNo, null, sms, null, null);
+                    Toast.makeText(getApplicationContext(), "SMS Sent!",
+                            Toast.LENGTH_LONG).show();
+                    editSMS.setText("");
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(),
+                            "SMS failed, please try again later!",
+                            Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+                }
+
+            }
+        });
     }
 
 
@@ -33,6 +71,7 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         //MenuInflater inflater = getMenuInflater();
         //getMenuInflater().inflate(R.menu.menu_main, menu);
+
         return super.onCreateOptionsMenu(menu);
     }
 
